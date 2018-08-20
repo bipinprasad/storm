a9bc7b8e195a71c036427610d645da8677c91381
Robert (Bobby) Evans <evans@yahoo-inc.com>
2018-08-20 10:07:28 -0500

    YSTORM-5320: fix issues with SpoutLag

--- storm-core/src/jvm/org/apache/storm/utils/TopologySpoutLag.java
+++ storm-core/src/jvm/org/apache/storm/utils/TopologySpoutLag.java
@@ -34,6 +34,7 @@ public class TopologySpoutLag {
     private static final String SPOUT_TYPE = "spoutType";
     private static final String SPOUT_LAG_RESULT = "spoutLagResult";
     private static final String ERROR_INFO = "errorInfo";
+    private static final String CONFIG_KEY_PREFIX = "config.";
     private final static Logger logger = LoggerFactory.getLogger(TopologySpoutLag.class);
 
     public static Map<String, Map<String, Object>> lag(StormTopology stormTopology, Map<String, Object> topologyConf) {
@@ -43,13 +44,7 @@ public class TopologySpoutLag {
         for (Map.Entry<String, SpoutSpec> spout : spouts.entrySet()) {
             try {
                 SpoutSpec spoutSpec = spout.getValue();
-                ComponentObject componentObject = spoutSpec.get_spout_object();
-                // FIXME: yes it's a trick so we might be better to find alternative way...
-                className = getClassNameFromComponentObject(componentObject);
-                logger.debug("spout classname: {}", className);
-                if (className.endsWith("storm.kafka.spout.KafkaSpout")) {
-                    result.put(spout.getKey(), getLagResultForNewKafkaSpout(spout.getKey(), spoutSpec));
-                }
+                addLagResultForKafkaSpout(result, spout.getKey(), spoutSpec);
             } catch (Exception e) {
                 logger.warn("Exception thrown while getting lag for spout id: " + spout.getKey() + " and spout class: " + className);
                 logger.warn("Exception message:" + e.getMessage(), e);
@@ -58,63 +53,52 @@ public class TopologySpoutLag {
         return result;
     }
 
-    private static String getClassNameFromComponentObject(ComponentObject componentObject) {
-        try {
-            Object object = Utils.getSetComponentObject(componentObject);
-            return object.getClass().getCanonicalName();
-        } catch (RuntimeException e) {
-
-            if (e.getCause() instanceof ClassNotFoundException) {
-                return e.getCause().getMessage().trim();
-            }
-
-            throw e;
-        }
-    }
-
     private static List<String> getCommandLineOptionsForNewKafkaSpout(Map<String, Object> jsonConf) {
         logger.debug("json configuration: {}", jsonConf);
 
         List<String> commands = new ArrayList<>();
-        String configKeyPrefix = "config.";
         commands.add("-t");
-        commands.add((String) jsonConf.get(configKeyPrefix + "topics"));
+        commands.add((String) jsonConf.get(CONFIG_KEY_PREFIX + "topics"));
         commands.add("-g");
-        commands.add((String) jsonConf.get(configKeyPrefix + "groupid"));
+        commands.add((String) jsonConf.get(CONFIG_KEY_PREFIX + "groupid"));
         commands.add("-b");
-        commands.add((String) jsonConf.get(configKeyPrefix + "bootstrap.servers"));
-        String securityProtocol = (String) jsonConf.get(configKeyPrefix + "security.protocol");
+        commands.add((String) jsonConf.get(CONFIG_KEY_PREFIX + "bootstrap.servers"));
+        String securityProtocol = (String) jsonConf.get(CONFIG_KEY_PREFIX + "security.protocol");
         if (securityProtocol != null && !securityProtocol.isEmpty()) {
             commands.add("-s");
             commands.add(securityProtocol);
         }
         return commands;
     }
-    
-    private static Map<String, Object> getLagResultForKafka(String spoutId, SpoutSpec spoutSpec) throws IOException {
+
+    private static void addLagResultForKafkaSpout(Map<String, Map<String, Object>> finalResult, String spoutId, SpoutSpec spoutSpec)
+        throws IOException {
         ComponentCommon componentCommon = spoutSpec.get_common();
         String json = componentCommon.get_json_conf();
-        Map<String, Object> result = null;
-        String errorMsg = "Offset lags for kafka not supported for older versions. Please update kafka spout to latest version.";
         if (json != null && !json.isEmpty()) {
-            List<String> commands = new ArrayList<>();
-            String stormHomeDir = System.getenv("STORM_BASE_DIR");
-            if (stormHomeDir != null && !stormHomeDir.endsWith("/")) {
-                stormHomeDir += File.separator;
-            }
-            commands.add(stormHomeDir != null ? stormHomeDir + "bin" + File.separator + "storm-kafka-monitor" : "storm-kafka-monitor");
             Map<String, Object> jsonMap = null;
             try {
                 jsonMap = (Map<String, Object>) JSONValue.parseWithException(json);
             } catch (ParseException e) {
                 throw new IOException(e);
             }
-            commands.addAll(getCommandLineOptionsForNewKafkaSpout(jsonMap));
 
-            logger.debug("Command to run: {}", commands);
+            if (jsonMap.containsKey(CONFIG_KEY_PREFIX + "topics")
+                && jsonMap.containsKey(CONFIG_KEY_PREFIX + "groupid")
+                && jsonMap.containsKey(CONFIG_KEY_PREFIX + "bootstrap.servers")) {
 
-            // if commands contains one or more null value, spout is compiled with lower version of storm-kafka-client
-            if (!commands.contains(null)) {
+                Map<String, Object> result = null;
+                List<String> commands = new ArrayList<>();
+                String stormHomeDir = System.getenv("STORM_BASE_DIR");
+                if (stormHomeDir != null && !stormHomeDir.endsWith("/")) {
+                    stormHomeDir += File.separator;
+                }
+                commands.add(stormHomeDir != null ? stormHomeDir + "bin" + File.separator + "storm-kafka-monitor" : "storm-kafka-monitor");
+                commands.addAll(getCommandLineOptionsForNewKafkaSpout(jsonMap));
+                String errorMsg = "INTERNAL ERROR";
+
+                logger.debug("Command to run: {}", commands);
+
                 String resultFromMonitor = new ShellCommandRunnerImpl().execCommand(commands.toArray(new String[0]));
 
                 try {
@@ -124,23 +108,19 @@ public class TopologySpoutLag {
                     // json parsing fail -> error received
                     errorMsg = resultFromMonitor;
                 }
+
+                Map<String, Object> kafkaSpoutLagInfo = new HashMap<>();
+                kafkaSpoutLagInfo.put(SPOUT_ID, spoutId);
+                kafkaSpoutLagInfo.put(SPOUT_TYPE, "KAFKA");
+
+                if (result != null) {
+                    kafkaSpoutLagInfo.put(SPOUT_LAG_RESULT, result);
+                } else {
+                    kafkaSpoutLagInfo.put(ERROR_INFO, errorMsg);
+                }
+
+                finalResult.put(spoutId, kafkaSpoutLagInfo);
             }
         }
-
-        Map<String, Object> kafkaSpoutLagInfo = new HashMap<>();
-        kafkaSpoutLagInfo.put(SPOUT_ID, spoutId);
-        kafkaSpoutLagInfo.put(SPOUT_TYPE, "KAFKA");
-
-        if (result != null) {
-            kafkaSpoutLagInfo.put(SPOUT_LAG_RESULT, result);
-        } else {
-            kafkaSpoutLagInfo.put(ERROR_INFO, errorMsg);
-        }
-
-        return kafkaSpoutLagInfo;
-    }
-
-    private static Map<String, Object> getLagResultForNewKafkaSpout(String spoutId, SpoutSpec spoutSpec) throws IOException {
-        return getLagResultForKafka(spoutId, spoutSpec);
     }
 }
